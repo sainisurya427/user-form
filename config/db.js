c4dffa7bb6ac2b0676a6aaab1bbb4a4ebb7e16e7
saurabh saini <suryasaini427@gmail.com>
2024-08-20 19:59:04 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,29 +1,3 @@
-// // config/db.js
-// const mongoose = require('mongoose');
-
-
-// //const uri = `mongodb+srv://sssaini427:eUcyhWUTvdZwDjY0@saurabh.j5d6osx.mongodb.net/?retryWrites=true&w=majority&appName=Saurabh`;
-// // process.env.MONGODB_URI
-//  const uri = `mongodb+srv://sssaini427:eUcyhWUTvdZwDjY0@saurabh.j5d6osx.mongodb.net/mydata`;
-
-// mongoose.connect(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }).then(() => {
-//     console.log('Connected to MongoDB');
-// }).catch(err => {
-//     console.error('Failed to connect to MongoDB:', err.message);
-// });
-
-// module.exports = mongoose;
-
-
-
-
-
-
-
-
 
 const mongoose = require('mongoose');
 require('dotenv').config(); // Only necessary for local development with a .env file
