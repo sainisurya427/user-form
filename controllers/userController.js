233a800ba3668aa21d16daf594946b729893b35a
saurabh saini <suryasaini427@gmail.com>
2024-08-21 19:58:05 -0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,16 @@ exports.saveUser = async (req, res) => {
     try {
         const user = new User(req.body);
         const savedUser = await user.save();
+
+                // Emit the 'userRegistered' event via socket.io
+        const io = req.app.get('socketio');
+        io.emit('userRegistered', {
+            email: savedUser.email,
+            name: `${savedUser.firstName} ${savedUser.lastName}`,
+            socketId: req.headers['socket-id']  // Optionally pass socket ID if needed
+        });
+
+        
         res.status(201).json(savedUser);
     } catch (error) {
         res.status(400).json({ message: error.message });
